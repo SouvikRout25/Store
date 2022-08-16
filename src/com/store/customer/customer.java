223a308542dfb6a3cc37ceb1a8594d81f8a123b9
SOUVIK ROUT <souvikrout.21@gmail.com>
2022-08-16 13:23:53 +0530

    initial commit

--- src/com/store/customer/customer.java
+++ src/com/store/customer/customer.java
@@ -1,5 +1,49 @@
 package com.store.customer;
 
 public class customer {
+private String Name;
+private String Email;
+private String Address;
+private long mobile;
+private boolean membership;
 
+    public String getName() {
+        return Name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public boolean isMembership() {
+        return membership;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+
+    public void setMembership(boolean membership) {
+        this.membership = membership;
+    }
 }
---
+++ src/com/store/customer/customerImpl.java
@@ -0,0 +1,4 @@
+package com.store.customer;
+
+public class customerImpl {
+}
